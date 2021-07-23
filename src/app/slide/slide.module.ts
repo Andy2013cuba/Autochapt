35e499c0b0745bb592f1e6c8c4a3fb42ecf03713
andiel <andiel@tic.copextel.com.cu>
2021-07-23 01:34:17 -0600

    ultimos arreglos

--- src/app/slide/slide.module.ts
+++ src/app/slide/slide.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from '@angular/common';
 
-import { AppComponent } from "./app.component";
+
 import { SwiperModule } from "swiper/angular";
 
 @NgModule({
-  imports: [BrowserModule, CommonModule, FormsModule, SwiperModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, CommonModule, FormsModule, SwiperModule],  
+  
 })
 export class AppModule {}
